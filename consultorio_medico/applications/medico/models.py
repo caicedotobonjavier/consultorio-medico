987ef5997a52994e4e036135f4305381bd51f1d7
JavierCaicedoTobon <javiercaicedotobon@gmail.com>
2025-12-08 15:38:06 -0500

    se crean los modelos de la app medico

--- consultorio_medico/applications/medico/models.py
+++ consultorio_medico/applications/medico/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+#
+from model_utils.models import TimeStampedModel
+#
+from applications.users.models import User
 # Create your models here.
+
+class Especialidad(TimeStampedModel):
+    nombre = models.CharField('Nombre', max_length=120)
+
+
+    class Meta:
+        verbose_name = 'Especilidad'
+        verbose_name_plural = 'Especialidades'
+        ordering = ['id']
+    
+
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Medico(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.SET_NULL, null=True)
+    registro_profesional = models.CharField('Registro Profesional', max_length=50)
+
+
+    class Meta:
+        verbose_name = 'Medico'
+        verbose_name_plural = 'Medicos'
+        ordering = ['id']
+    
+
+    def __str__(self):
+        return f'{self.user} {self.especialidad}'
