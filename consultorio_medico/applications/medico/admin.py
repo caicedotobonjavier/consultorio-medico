e9d04ad027dd5179dbc7b5b750f5b39f6a1a7a0e
JavierCaicedoTobon <javiercaicedotobon@gmail.com>
2025-12-08 16:41:51 -0500

    se crean los admin para medico

--- consultorio_medico/applications/medico/admin.py
+++ consultorio_medico/applications/medico/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+#
+from .models import Medico, Especialidad
 # Register your models here.
+
+
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'especialidad',
+        'registro_profesional',
+    )
+
+
+admin.site.register(Medico, MedicoAdmin)
+
+
+
+class EspecialidadAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+    )
+
+
+admin.site.register(Especialidad, EspecialidadAdmin)
